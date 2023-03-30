ccda3d07dc45307fe27427cee80fd6c35ca64794
Masato Siraisi <zhaitengkong5@gmail.com>
2023-03-31 01:59:51 +0900

    KwatchAPIのnullレスポンスへの対応措置

--- ShindoLevel/Form1.cs
+++ ShindoLevel/Form1.cs
@@ -33,18 +33,37 @@ namespace ShindoLevel
 
         private async void timer1_Tick(object sender, EventArgs e)
         {
-            var url = "https://kwatch-24h.net/EQLevel.json";
-            var json = await client.GetStringAsync(url);
-            var s_lv = JsonConvert.DeserializeObject<Root>(json);
+            try
+            {
+                var url = "https://kwatch-24h.net/EQLevel.json";
+                var json = await client.GetStringAsync(url);
+                var s_lv = JsonConvert.DeserializeObject<Root>(json);
+                if (s_lv != null)
+                {
+                    var lv = s_lv.l;//←振動レベル本体
+                    var green = s_lv.g;//←緑点
+                    var yellow = s_lv.y;//←黄点
+                    var red = s_lv.r;//←赤点
+                    var eew_ = s_lv.e;//←何の内容なのかは未検証
+                    var prac = s_lv.p;//←地震検知時のみ現れる。内容は数字で都道府県番号に合うようになっている。
 
-            var lv = s_lv.l;//←振動レベル本体
-            var green = s_lv.g;//←緑点
-            var yellow = s_lv.y;//←黄点
-            var red = s_lv.r;//←赤点
-            var eew_ = s_lv.e;//←何の内容なのかは未検証
-            var prac = s_lv.p;//←地震検知時のみ現れる。内容は数字で都道府県番号に合うようになっている。
-
-            label1.Text = "振動レベル:Lv"+ lv.ToString();
+                    label1.Text = "振動レベル:Lv"+ lv.ToString();
+                }
+                else
+                {
+                    goto onError;
+                }
+            }
+            catch
+            {
+                goto onError;
+            }
+            return;
+        onError:
+            timer1.Enabled=false;
+            label1.Text = "振動レベル:Lv ----";
+            await Task.Delay(100);
+            timer1.Enabled = true;
         }
     }
 }
